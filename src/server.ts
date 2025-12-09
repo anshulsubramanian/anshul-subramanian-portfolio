c07d50d608646e3678c2d774d83efbb0216122a6
Anshul Subramanian <anshulsubramanian@gmail.com>
2025-12-09 23:44:09 +0530

    Adding New Achievement

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 // Serve static files from public directory
 app.use(express.static(path.join(__dirname, '../public')));
